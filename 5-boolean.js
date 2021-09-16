0ddd9c41d70cc4dbdbb92637fa3afec958ad0459
salentim <rodrigosalentim@gmail.com>
2021-09-16 13:22:32 -0400

    Arquivo Boolean

---
+++ 5-boolean.js
@@ -0,0 +1,11 @@
+const ativo = true
+
+const inativo = false
+
+// valor booelan true
+console.log(typeof ativo)
+console.log(ativo)
+
+// valor booelan false
+console.log(typeof inativo)
+console.log(inativo)
